e0dfc39c0df263d6a95a0571b526ff95da821107
poluzerov <poluzerov.andrey@mail.ru>
2025-09-08 14:52:52 +0300

    add proga.cpp

---
+++ proga.cpp
@@ -0,0 +1,21 @@
+#include<fstream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+int main(){
+    srand(time(0));
+    const char* filenames[] = {"file1.txt", "file2.txt", "file2.txt"};
+    const int countNumbers = 10;
+    for (const char* filename : filenames){
+        ofstream file(filename);
+        if (!file.is_open()) return 1;
+        for (int i = 0; i < countNumbers; i++){
+            int number = rand()%10+1;
+            file << number;
+            if (i < countNumbers - 1) file << " ";
+        }
+    }
+    return 0;
+}
